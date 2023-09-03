7822be80904dbc44889df238198c9fa72ab05529
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-09-03 05:05:37 +0300

    [FEATURE] App Theme

---
+++ lib/core/themes/app_theme.dart
@@ -0,0 +1,45 @@
+import 'package:chef_app/core/utils/app_colors.dart';
+import 'package:chef_app/core/utils/app_text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData getAppTheme() {
+  return ThemeData(
+    primaryColor: AppColors.primaryColor,
+    //scaffoldBackgroundColor
+    scaffoldBackgroundColor: AppColors.whiteColor,
+    //appBar theme
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.primaryColor,
+    ),
+    //text theme
+    textTheme: TextTheme(
+      displayLarge: boldStyle(),
+      displayMedium: boldStyle(),
+    ),
+    //button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+      ),
+    ),
+    //Text Field
+    inputDecorationTheme: InputDecorationTheme(
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      hintStyle: boldStyle(
+        color: AppColors.greyColor,
+        fontSize: 16,
+      ),
+      // fillColor: AppColors.whiteColor,
+      // filled: true,
+    ),
+  );
+}
