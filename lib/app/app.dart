a6eae593255a2db4c03465fed09facb0c4daca72
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-09-03 20:28:01 +0300

    [FEATURE] Splash Screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,18 +1,22 @@
 import 'package:chef_app/core/routes/app_routes.dart';
 import 'package:chef_app/core/themes/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chef-Application',
-      debugShowCheckedModeBanner: false,
-      initialRoute: Routes.initialRoute,
-      onGenerateRoute: AppRoutes.onGenerateRoutes,
-      theme: getAppTheme(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, child) => MaterialApp(
+        title: 'Chef-Application',
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routes.initialRoute,
+        onGenerateRoute: AppRoutes.onGenerateRoutes,
+        theme: getAppTheme(),
+      ),
     );
   }
 }
--- lib/features/auth/presentation/screens/splash_screen.dart
+++ lib/features/auth/presentation/screens/splash_screen.dart
@@ -1,10 +1,36 @@
+import 'package:chef_app/core/utils/app_assets.dart';
+import 'package:chef_app/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 120.h,
+              width: 120.w,
+              child: Image.asset(
+                AppAssets.appLogo,
+              ),
+            ),
+            SizedBox(
+              height: 16.h,
+            ),
+            Text(
+              "Chef APP",
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
