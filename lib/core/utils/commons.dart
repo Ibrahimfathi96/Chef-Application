2cf96ab793499bd4d17c05cd1af4a661b010f35f
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-09-03 20:39:38 +0300

    [FEATURE] Navigate to change Language Screen

---
+++ lib/core/utils/commons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void navigate({
+  required BuildContext context,
+  required String routeName,
+  dynamic arguments,
+}) {
+  Navigator.pushNamed(context, routeName, arguments: arguments);
+}
--- lib/features/auth/presentation/screens/splash_screen.dart
+++ lib/features/auth/presentation/screens/splash_screen.dart
@@ -1,10 +1,32 @@
+import 'package:chef_app/core/routes/app_routes.dart';
 import 'package:chef_app/core/utils/app_assets.dart';
 import 'package:chef_app/core/utils/app_colors.dart';
+import 'package:chef_app/core/utils/commons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    navigateAfterThreeSeconds();
+    super.initState();
+  }
+
+  void navigateAfterThreeSeconds() {
+    Future.delayed(const Duration(seconds: 3)).then(
+      (value) => navigate(
+        context: context,
+        routeName: Routes.changeLang,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +48,9 @@ class SplashScreen extends StatelessWidget {
             ),
             Text(
               "Chef APP",
-              style: Theme.of(context).textTheme.displayLarge,
+              style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                    color: AppColors.blackColor,
+                  ),
             ),
           ],
         ),
