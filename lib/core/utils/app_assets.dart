dbbe67f6c45f883aa4ed29cc4d44241d91e8efe0
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-09-03 03:52:38 +0300

    [FEATURE] App Assets & Colors

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,13 @@
+class AppAssets {
+  static const String basePath = "assets/images";
+  static const String appLogo = '$basePath/logo.png';
+  static const String lock = '$basePath/lock.png';
+  static const String imagePicker = '$basePath/image_picker.png';
+  static const String background = '$basePath/bg.png';
+  static const String background1 = '$basePath/bg1.png';
+  static const String appLogoSVG = '$basePath/logo.svg';
+  static const String lockSVG = '$basePath/lock.svg';
+  static const String imagePickerSVG = '$basePath/image_picker.svg';
+  static const String backgroundSVG = '$basePath/bg.svg';
+  static const String background1SVG = '$basePath/bg1.svg';
+}
