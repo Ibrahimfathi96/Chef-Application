32c31446f9674ae4e3584cf35012ef9a022f2afe
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-09-09 21:36:33 +0300

    [FEATURE]  Adding Shimmer Effect

--- lib/core/widgets/custom_cached_network_image.dart
+++ lib/core/widgets/custom_cached_network_image.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chef_app/core/widgets/custom_loading_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CustomCachedNetworkImage extends StatelessWidget {
   final String imageUrl;
@@ -15,7 +17,18 @@ class CustomCachedNetworkImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return CachedNetworkImage(
       fit: fit,
-      placeholder: (context, url) => const CustomLoadingIndicator(),
+      placeholder: (context, url) => Shimmer.fromColors(
+        baseColor: Colors.grey[850]!,
+        highlightColor: Colors.grey[800]!,
+        child: Container(
+          height: 60.h,
+          width: 60.w,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ),
       errorWidget: (context, url, error) => const Icon(Icons.error),
       imageUrl: imageUrl,
     );
